5f58e138c3d58d8504ef4b88237d687ab16d5440
rifqiyy19 <rifqicpz@gmail.com>
2025-10-24 15:06:00 +0700

    Perubahan kedua

--- JavaVersionnControlPractice/src/Main.java
+++ JavaVersionnControlPractice/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello word!");
+        String nama = "Rifqi";
+        System.out.println("Hello " + nama);
+
     }
 }
